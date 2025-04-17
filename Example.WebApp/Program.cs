a7575226ca9007f18080c6dac5c5dbba1445b659
Adam Gartee <agartee@gmail.com>
2025-04-17 16:06:22 -0400

    added authentication

--- Example.WebApp/Program.cs
+++ Example.WebApp/Program.cs
@@ -1,9 +1,60 @@
 using Example.WebApp.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var authConfig = builder.Configuration.GetSection("Authentication");
+var authority = authConfig["Authority"];
+var clientId = authConfig["ClientId"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authority;
+        options.Audience = clientId;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name"
+        };
+    });
+
+builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Example API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Enter a valid JWT Bearer token",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "bearer",
+                Name = "Bearer",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -34,6 +85,6 @@ app.MapGet("api/example", (HttpContext httpContext) =>
     };
 
     return data;
-});
+}).RequireAuthorization();
 
 app.Run();
---
+++ README.md
@@ -0,0 +1,10 @@
+User Secrets / App Settings
+
+```json
+{
+  "Authentication": {
+    "Authority": "https://{YOUR_DOMAIN}.b2clogin.com/garteespikes.onmicrosoft.com/B2C_1_signin_signup",
+    "ClientId": "{CLIENT_ID}"
+  }
+}
+```
