c7927651e3e1949582eabc0cf7c74cbcc46060b8
Adam Gartee <agartee@gmail.com>
2025-04-17 14:31:54 -0400

    added httpContext to example endpoint

--- Example.WebApp/Program.cs
+++ Example.WebApp/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("api/example", () =>
+app.MapGet("api/example", (HttpContext httpContext) =>
 {
     var data = new[]
     {
