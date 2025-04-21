e85a978c9de32dabf745047a83356bc7e129da4c
Adam Gartee <agartee@gmail.com>
2025-04-21 10:41:30 -0400

    added CORS config

--- Example.WebApp/Program.cs
+++ Example.WebApp/Program.cs
@@ -5,11 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAppAuthentication(builder.Configuration);
 builder.Services.AddAppAuthorization();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddAppSwagger();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
